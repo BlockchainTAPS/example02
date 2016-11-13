be6e26d87379b7c731ae58a6e9c7c982d973b4bd
JasonThyeMC <munchun.thye@gmail.com>
2016-11-13 12:36:46 +0800

    Remove shim interface

--- chaincode/chaincode_example02.go
+++ chaincode/chaincode_example02.go
@@ -38,7 +38,7 @@ func main() {
 	}
 }
 
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Printf("Init called, initializing chaincode")
 	
 	var A, B string    // Entities
